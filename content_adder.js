cd3ab08146b5a25b4340de9533d7972e66fbf5f6
gauravdilkash091 <53783823+gauravdilkash091@users.noreply.github.com>
2019-10-20 01:36:39 -0700

    second time uploading

--- content_adder.js
+++ content_adder.js
@@ -1,1 +1,22 @@
 console.log('see its working');
+function user_validation(){
+	var x=document.getElementsByClassName('user-content.html');
+	var bool=false;
+	for(var i=0;i<x.length;i++){
+		if(x[i].value==''){
+			alert('please fill all the details!!!');
+			break;
+		}
+		else{bool=true;}
+	}
+	if(bool){
+		var temp=x[0].value;
+		window.location='index.html';
+		var img=document.createElement('img');
+			img.setAttribute('src',temp);
+			img.setAttribute('alt', 'file not found');
+			img.className='userimg';
+			var add=document.getElementById('user_provided_content');
+			add.appendChild(img);
+	}
+}
